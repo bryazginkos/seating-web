b176f775d496ad12f263f51950717a8da8b47fa6
konstantin <bryazginkos@yahoo.com>
2015-01-07 00:52:46 +0600

    error module fix

--- src/main/java/ru/seating/web/client/error/ErrorPageView.java
+++ src/main/java/ru/seating/web/client/error/ErrorPageView.java
@@ -4,10 +4,17 @@ import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.user.client.ui.Widget;
 import com.gwtplatform.mvp.client.ViewImpl;
 
+import javax.inject.Inject;
+
 /**
  * Created by Константин on 06.01.2015.
  */
 public class ErrorPageView extends ViewImpl implements ErrorPagePresenter.MyView {
     public interface Binder extends UiBinder<Widget, ErrorPageView> {
+    }
+
+    @Inject
+    ErrorPageView(Binder uiBinder) {
+        initWidget(uiBinder.createAndBindUi(this));
     }
 }
