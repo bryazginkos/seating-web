0f56f34420bda8ac61f473bfb1310339e51baf57
konstantin <bryazginkos@yahoo.com>
2015-01-06 16:01:23 +0600

    change nameToken's name

--- src/main/java/ru/seating/web/client/place/NameTokens.java
+++ src/main/java/ru/seating/web/client/place/NameTokens.java
@@ -2,7 +2,7 @@ package ru.seating.web.client.place;
 
 public class NameTokens {
     public static final String home = "!home";
-    public static final String persons = "persons";
+    public static final String persons = "!persons";
 
     public static String getHome() {
         return home;
